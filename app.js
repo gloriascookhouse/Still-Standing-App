958119978dc7a61d63c74d60bfe050d6352a0a0b
Gloria's Cookhouse <diamanteduruibe512@gmail.com>
2026-02-10 17:21:58 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// Load daily message
+fetch('./content/messages.json')
+  .then(res => res.json())
+  .then(data => {
+    const activeMessage = data.find(msg => msg.active);
+    if (activeMessage) {
+      document.getElementById('dailyMessage').innerText = activeMessage.message;
+    }
+  })
+  .catch(err => console.error('Error loading messages:', err));
+
+// Load articles
+fetch('./content/articles.json')
+  .then(res => res.json())
+  .then(articles => {
+    const container = document.getElementById('articles');
+    articles.forEach(a => {
+      const div = document.createElement('div');
+      div.innerHTML = `<h3>${a.title}</h3><p>${a.body}</p>`;
+      container.appendChild(div);
+    });
+  })
+  .catch(err => console.error('Error loading articles:', err));
+
+// Load calm content
+fetch('./content/calm.json')
+  .then(res => res.json())
+  .then(calm => {
+    document.getElementById('calmTitle').innerText = calm.title;
+    document.getElementById('calmBody').innerText = calm.body;
+  })
+  .catch(err => console.error('Error loading calm content:', err));
