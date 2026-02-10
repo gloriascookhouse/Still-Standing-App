3dd596c598a0617f7ece9d6aa29bede6a5e8503c
Gloria's Cookhouse <diamanteduruibe512@gmail.com>
2026-02-10 17:46:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
-// Load daily message
+// GLOBAL VARIABLES
+let currentMessageIndex = 0;
+let messagesData = [];
+let calmData = [];
+
+// ---------- LOAD MESSAGES ----------
 fetch('./messages.json')
   .then(res => res.json())
   .then(data => {
-    const activeMessage = data.find(msg => msg.active);
-    if (activeMessage) {
-      document.getElementById('dailyMessage').innerText = activeMessage.message;
-    }
+    messagesData = data;
+    // Find first active message index
+    const activeIndex = messagesData.findIndex(msg => msg.active);
+    currentMessageIndex = activeIndex !== -1 ? activeIndex : 0;
+    showMessage(currentMessageIndex);
   })
   .catch(err => console.error('Error loading messages:', err));
 
-// Load articles
+function showMessage(index) {
+  const msg = messagesData[index];
+  if (msg) {
+    document.getElementById('dailyMessage').innerText = msg.message;
+  }
+}
+
+// Next Message Button
+const nextBtn = document.createElement('button');
+nextBtn.innerText = 'Next Message';
+nextBtn.onclick = () => {
+  currentMessageIndex = (currentMessageIndex + 1) % messagesData.length;
+  showMessage(currentMessageIndex);
+};
+document.body.insertBefore(nextBtn, document.getElementById('articles'));
+
+// ---------- LOAD ARTICLES ----------
 fetch('./articles.json')
   .then(res => res.json())
   .then(articles => {
@@ -22,11 +44,24 @@ fetch('./articles.json')
   })
   .catch(err => console.error('Error loading articles:', err));
 
-// Load calm content
+// ---------- LOAD CALM CONTENT ----------
 fetch('./calm.json')
   .then(res => res.json())
-  .then(calm => {
-    document.getElementById('calmTitle').innerText = calm.title;
-    document.getElementById('calmBody').innerText = calm.body;
+  .then(data => {
+    calmData = [data]; // wrap single calm tip in array
+    showCalm();
   })
   .catch(err => console.error('Error loading calm content:', err));
+
+function showCalm() {
+  const calmIndex = Math.floor(Math.random() * calmData.length);
+  const calm = calmData[calmIndex];
+  document.getElementById('calmTitle').innerText = calm.title;
+  document.getElementById('calmBody').innerText = calm.body;
+}
+
+// Random Calm Button
+const calmBtn = document.createElement('button');
+calmBtn.innerText = 'New Calm Tip';
+calmBtn.onclick = showCalm;
+document.body.insertBefore(calmBtn, document.getElementById('calmBody'));
