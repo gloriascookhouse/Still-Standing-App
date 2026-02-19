0274569eea4c15b1cf4b7cae678919892170ec85
Diamante0 <diamanteduruibe512@gmail.com>
2026-02-19 20:42:10 +0100

    Ready for hosting backend

--- server.js
+++ server.js
@@ -24,3 +24,5 @@ mongoose.connect(process.env.MONGO_URI)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const cors = require('cors');
+app.use(cors({ origin: '*' })); // allow requests from any origin for now
